0dd617e55ee85d05df853e9f7ac5a2484ecaa76f
Juanjo <juanjotr31@gmail.com>
2025-02-10 20:47:18 +0100

    Tercer commit

--- controller/digimonController.js
+++ controller/digimonController.js
@@ -7,35 +7,41 @@ class digimonController{
     }
 
     async index(req, res){
-        let random = Math.floor(Math.random() * 1460);
-        let respuesta = [];
+        if(!session.digimon){
+            diario()
+        }
 
-        fetch(`https://digi-api.com/api/v1/digimon/${random}`)
-        .then(response => {
-            if(response.ok){
-                return response.json();
-            }
-        })
-        .then(respuesta => {
-            try{
-                let data = respuesta
-                console.log(data.name);
-                req.session.digimon = data
-                req.session.respuesta = respuesta;
+        function diario(){
+            let random = Math.floor(Math.random() * 1460);
+            fetch(`https://digi-api.com/api/v1/digimon/${random}`)
+            .then(response => {
+                if(response.ok){
+                    return response.json();
+                }
+            })
+            .then(response => {
+                try{
+                    let respuesta = [];
+                    let data = response
+                    console.log('Inicio ' + data.name);
+                    session.digimon = data
+                    session.respuesta = respuesta;
 
-                res.render('pages/index', {digimon: data})
-            }catch(e){
-                console.log(e.message);
-                res.status(500).send(e);
-            }
-        })
+                    res.render('pages/index', {digimon: data})
+                }catch(e){
+                    console.log(e.message);
+                    res.status(500).send(e);
+                }
+            })
+        }
     }
 
     async buscar(req, res){
         try{
             const { nombre } = req.body;
 
-            console.log(nombre, req.session.digimon, req.session.respuesta);
+            console.log('Nombre form: ' + nombre);
+            console.log('Sessions ' + session.digimon.name, session.respuesta);
 
             fetch(`https://digi-api.com/api/v1/digimon/${nombre}`)
             .then(response => {
@@ -44,7 +50,7 @@ class digimonController{
                 }
             })
             .then(response => {
-                console.log(response)
+                //console.log(response.name)
                 // console.log(response.name);
                 // console.log(digimon.name);
                 // if(response.name == digimon.name){
@@ -53,7 +59,7 @@ class digimonController{
                 //     respuesta.push(false)
                 // }
 
-                // res.redirect( '/digimon/', {respuesta: respuesta});
+                res.redirect( '/digimon/');
             })
         }catch(e){
             res.status(500).send(e);
